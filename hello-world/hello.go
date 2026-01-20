b0c77f418120eff80c01c013d168d5a5c560328b
fernando8franco <francoduranluisfernando@gmail.com>
2026-01-20 11:57:47 -0600

    hello finished

--- hello-world/hello.go
+++ hello-world/hello.go
@@ -1,16 +1,43 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
-const englishHelloPrefix = "Hello, "
+const (
+	spanish   = "Spanish"
+	french    = "French"
+	brazilian = "Brazilian"
 
-func Hello(name string) string {
+	englishHelloPrefix   = "Hello, "
+	spanishHelloPrefix   = "Hola, "
+	frenchhHelloPrefix   = "Bonjour, "
+	brazilianHelloPrefix = "Olá, "
+)
+
+func Hello(name, language string) string {
 	if name == "" {
 		name = "World"
 	}
-	return englishHelloPrefix + name
+
+	return greetingPrefix(language) + name
+}
+
+func greetingPrefix(language string) (prefix string) {
+	switch language {
+	case spanish:
+		prefix = spanishHelloPrefix
+	case french:
+		prefix = frenchhHelloPrefix
+	case brazilian:
+		prefix = brazilianHelloPrefix
+	default:
+		prefix = englishHelloPrefix
+	}
+
+	return
 }
 
 func main() {
-	fmt.Println(Hello("world"))
+	fmt.Println(Hello("world", ""))
 }
